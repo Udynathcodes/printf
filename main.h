39bbc336e2ce9097d3c5a0cf9fe916bdb4225c54
Michael Kanu <michaelokanu01@yahoo.com>
2023-11-13 07:13:43 -0800

    main update

--- main.h
+++ main.h
@@ -6,9 +6,12 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-int _printf(const char format, ...);
-int percent(va_list list_of_args, const char format);
-int print_char(char c);
-int _strings(charstr);
+int _putchar(char c);
+int _printf(const char *format, ...);
+int printf_support(va_list list_of_args, const char format);
+int char_print(char c);
+int string_print(char *str);
+int print_cent(char c);
+int print_int(int n);
 
 #endif
---
+++ putchar.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+  * _putchar - write a char
+  * @c: char
+  * Return: char
+  */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
